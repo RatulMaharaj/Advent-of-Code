e030e9f3467a6478d8301d5d0b2b5092899f9810
Ratul Maharaj <ratulmaharaj@gmail.com>
2021-12-15 01:50:26 +0200

    completed day 7

---
+++ 2021/7b.py
@@ -0,0 +1,24 @@
+# Advent of Code 2021
+# Day 7
+
+# Solution to part one
+
+import numpy as np
+
+with open("./7_data.txt", "r") as f:
+    data = np.array([int(item.strip()) for item in f.readlines()[0].split(",")])
+
+fuel_spend = {}
+
+
+def fuel_cost(n):
+    return n * (n + 1) / 2
+
+
+for x in range(0, max(data) + 1):
+    fuel_spend[f"{x}"] = sum(fuel_cost(abs(data - x)))
+
+
+print(
+    f"Optimum Position: {list(fuel_spend.keys())[list(fuel_spend.values()).index(min(fuel_spend.values()))]} \nFuel spend: {min(fuel_spend.values())}"
+)
